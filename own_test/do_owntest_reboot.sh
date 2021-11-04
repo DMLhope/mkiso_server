cb04ec4ae206a2f6be539116ad7a7ba4ff751e71
戴明亮； <daimingliang@uniontech.com>
2021-11-04 17:35:21 +0800

    整理自测脚本

--- own_test/do_owntest_reboot.sh
+++ own_test/do_owntest_reboot.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
+download_url="http://10.2.10.31/user:dml/uos-j-desk/owntest-tools/deepin-reboot-test.tar.gz"
+runtimes="$1"
+password="$2"
 
-wget -P ~/ http://10.2.10.31/user:dml/uos-j-desk/owntest-tools/deepin-reboot-test.tar.gz
+if [ "$runtimes" == "" ] || [ "$password" == "" ] ;then
+    echo "请提供重启次数及sudo密码"
+    echo "例如重启100次，密码123：bash do_owntest_reboot.sh 100 123"
+    exit
+fi
+
+if [ x"$3" != x"" ];then
+    echo "更换下载地址为：""$2"
+    download_url=$2
+fi
+
+wget -P ~/ "$download_url"
 
 tar -xf deepin-reboot-test.tar.gz
 
-cd deepin-reboot-test/
+cd deepin-reboot-test/ ||exit
 
-echo 123 |sudo -S bash deepin-reboot.sh init 200
+echo "$password" |sudo -S bash deepin-reboot.sh init "$runtimes"
