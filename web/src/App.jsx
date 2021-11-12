c2367ae782b0bae4dcf6a771c7369687c13ee97f
戴明亮； <daimingliang@uniontech.com>
2021-11-12 16:43:52 +0800

    添加前端页面

---
+++ web/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './index.css'
+import Mkiso from "./Mkiso";
+
+
+function App(){
+    return(
+        <Mkiso />
+    )
+}
+
+export default App;
---
+++ web/src/RecipeTitle.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function RecipeTitle(){
+    const title = '红烧肉';
+    return (
+        <h2>{ title }</h2>
+    )
+}
+
+export default RecipeTitle;
